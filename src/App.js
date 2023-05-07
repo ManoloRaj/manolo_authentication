f3dd16dcb50c027abff9961634af985897411a9f
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2023-05-07 12:11:16 +0300

    feat(routes): mise a jour de toutes les routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Outlet, Route, Routes, useLocation } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { HomePage } from './pages/HomePage';
+import AuthGuard from './helpers/AuthGuard';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router baseName="">
+        <Routes>
+          <Route path="" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/homepage" element={<AuthGuard children={<HomePage />} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
